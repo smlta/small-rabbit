a18e232c2ebe79c3032d641f21908f6dee392176
Alice <2162823020@qq.com>
2025-12-13 20:56:33 +0800

    完成新鲜好物和人气推荐组件的封装

--- src/views/Layout/children/RHome/RIndex.vue
+++ src/views/Layout/children/RHome/RIndex.vue
@@ -1,6 +1,7 @@
 <script setup>
   import HomeBanner from './components/HomeBanner.vue'
 import HomeCategory from './components/HomeCategory.vue'
+import HomePannel from './components/HomePannel.vue'
 </script>
 
 
@@ -9,6 +10,8 @@ import HomeCategory from './components/HomeCategory.vue'
   <HomeCategory></HomeCategory>
   <HomeBanner></HomeBanner>
 </div>
+<HomePannel title="新鲜好物" subTitle="新鲜出炉 品质靠谱"></HomePannel>
+<HomePannel title="人气推荐" subTitle="人气推荐 不容错过"></HomePannel>
 </template>
 
 <style scoped>
