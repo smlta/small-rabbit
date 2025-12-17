14552f7490584fb0fcb9cedc78964dbbe2227e92
Alice <2162823020@qq.com>
2025-12-17 20:03:52 +0800

    初步实现图片懒加载

--- src/main.js
+++ src/main.js
@@ -5,10 +5,20 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { useIntersectionObserver } from '@vueuse/core'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.directive('img-lazy',{
+  mounted(el,biding){
+    useIntersectionObserver(el,([{isIntersecting}]) => {
+      if(isIntersecting)
+      {
+        el.src = biding.value
+      } //当图片img元素进入视口区域,通过biding.value指令值获取图片链接加载图片
+    }) //该工具函数用来判断,监听的元素是否进入视口区域,如果进入isIntersecting的值会变为true
+  }
+})
 app.mount('#app')
